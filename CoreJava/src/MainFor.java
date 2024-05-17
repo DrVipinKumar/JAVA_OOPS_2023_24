ca748f21d7c5ed8a30b01d3173342b9fcfbc14f5
Dr Vipin Kumar <geniusvipin@gmail.com>
2024-05-17 11:36:48 +0530

    updated on 17/05/2024

--- CoreJava/src/MainFor.java
+++ CoreJava/src/MainFor.java
@@ -1,14 +1,23 @@
 class ForExample{
 	//int data[]= new int[10];
 	//int data[]= new int[] {12,34,56,77};
-	int data[]= {12,34,56,77};
+	 // int data1[][]=new int [2][];
+	int data[][]= {
+			         {12,34},
+			         {56,77}
+			        };
 	void displayFor()
 	{
 //	  for(int i=0;i<data.length;i++) {
-//		  System.out.println("data["+i+"]="+data[i]);
+//		  for (int j=0;j<data.length;j++)
+//		  {
+//		  System.out.println("data["+i+"]["+j+"]="+data[i][j]);
+//		  }
 //	  }
-		for(int value:data) {
-			System.out.println("Value="+value);
+		for(int value[]:data) {
+			for (int v:value) {
+			System.out.println("Value="+v);
+			}
 		}
 	}
 }
